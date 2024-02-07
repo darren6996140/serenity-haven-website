f5a11d4d7a3b44094cc7d9ab0f761cfa7aca6a7a
Darren Liau <liaukaize@gmail.com>
2024-02-07 16:18:30 +0800

    Update loginForm.php

--- loginForm.php
+++ loginForm.php
@@ -4,13 +4,19 @@
 <head>
 
     <style>
-        .loginForm {
-            background-color: rgba(235, 235, 235, 0.5);
-            padding-bottom: 20px;
-            border-radius: 5px;
+        .formContainer{
+            display: flex;
         }
 
-        .loginForm .title p {
+        .forms{
+            background-color: rgba(235, 235, 235, 0.5);
+            padding-bottom: 2%;
+            border-radius: 5px;
+            margin-left: 2%;
+            margin-right: 2%;
+        }
+
+        .forms .title p {
             font-size: 300%;
             font-weight: bold;
             text-align: center;
@@ -19,7 +25,7 @@
             padding: 10px 0px;
         }
 
-        .loginForm table {
+        .forms table {
             border: none;
             border-radius: 5px;
             border-collapse: collapse;
@@ -27,11 +33,11 @@
             background-color: rgb(235, 235, 235);
         }
 
-        .loginForm table, .loginForm td {
+        .forms table, .forms td {
             text-align: right;
         }
 
-        .loginForm .form {
+        .forms .form {
             padding: 10px;
             width: 200px;
             font-size: 100%;
@@ -39,7 +45,7 @@
             border-radius: 5px;
         }
 
-        .loginForm .button {
+        .forms .button {
             background-color: white;
             font-size: 150%;
             font-weight: bold;
@@ -48,15 +54,23 @@
             transition: 0.2s;
         }
 
-        .loginForm .button:hover {
+        .forms .button:hover {
             background-color: rgba(255, 255, 255, 0.5);
         }
 
-        .loginForm a {
+        .forms a {
             font-size: 120%;
             font-weight: bold;
             text-decoration: underline;
             color: rgb(26, 140, 255);
+        }
+
+        #registration{
+            flex: 50%;
+        }
+
+        #login{
+            flex: 50%;
         }
 
     </style>
@@ -69,75 +83,164 @@
     include("header.php");
     ?>
 
-    <div class="loginForm">
+    <div class="formContainer">
+        
+        <div class="forms" id="registration">
+            <form action="loginVisitorProcess.php" method="POST">
+                <div class="title">
+                    <p>Visitor Registration</p>
+                </div>
+                <table cellpadding=5px>
+                    <tr>
+                        <td style="width: 20px"></td>
+                        <td></td>
+                        <td></td>
+                        <td style="width: 20px"></td>
+                    </tr>
 
-        <form action="loginProcess.php" method="POST">
-            <div class="title">
-                <p>Log In</p>
-            </div>
-            <table cellpadding=5px>
-                <tr>
-                    <td style="width: 20px"></td>
-                    <td></td>
-                    <td></td>
-                    <td style="width: 20px"></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
 
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td>Email :</td>
+                        <td><input type="email" name="email" placeholder="email@email.com" class="form" required></td>
+                        <td></td>
+                    </tr>
 
-                <tr>
-                    <td></td>
-                    <td>Email :</td>
-                    <td><input type="text" name="email" class="form" required></td>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td>Name :</td>
+                        <td><input type="text" name="name" placeholder="Batman bin Suparman" class="form" required></td>
+                        <td></td>
+                    </tr>
 
-                <tr>
-                    <td></td>
-                    <td>Password :</td>
-                    <td><input type="password" name="password" class="form" required></td>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td>Unit Num. :</td>
+                        <td><input type="text" name="noUnit" placeholder="C20-01" class="form" required></td>
+                        <td></td>
+                    </tr>
 
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td><input type="submit" name="login" value="Log In" class="button"></td>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td>Phone Number :</td>
+                        <td><input type="text" name="noPhone" placeholder="0121234567" class="form" required></td>
+                        <td></td>
+                    </tr>
 
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td><a href="userForm.php">Register As New User</a></td>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td>Car Plate :</td>
+                        <td><input type="text" name="carPlat" placeholder="WYS6691" class="form" required></td>
+                        <td></td>
+                    </tr>
 
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td><input type="submit" name="login" value="Log In" class="button"></td>
+                        <td></td>
+                    </tr>
 
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td><a href="index.php">Back To Main Menu</a></td>
+                        <td></td>
+                    </tr>
 
-            </table>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
 
-        </form>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+
+        <div class="forms" id="login">
+
+            <form action="loginProcess.php" method="POST">
+                <div class="title">
+                    <p>Resident Log In</p>
+                </div>
+                <table cellpadding=5px>
+                    <tr>
+                        <td style="width: 20px"></td>
+                        <td></td>
+                        <td></td>
+                        <td style="width: 20px"></td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td>Email :</td>
+                        <td><input type="text" name="email" class="form" required></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td>Password :</td>
+                        <td><input type="password" name="password" class="form" required></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td><input type="submit" name="login" value="Log In" class="button"></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td><a href="userForm.php">Register As New User</a></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+                </table>
+
+            </form>
+
+        </div>
 
     </div>
-
     <?php
     include("footer.php");
     ?>
